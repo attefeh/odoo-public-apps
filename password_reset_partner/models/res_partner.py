0a6120be6a2df50eb069ca6dd1512032f2f4009b
Attefeh Falah <attefehfalah@gmail.com>
2026-05-27 19:29:02 +0400

    ready to use

---
+++ password_reset_partner/models/res_partner.py
@@ -0,0 +1,20 @@
+from odoo import models, _
+from odoo.exceptions import UserError
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    def reset_user_password(self):
+        self.ensure_one()
+        if not self.user_ids:
+            raise UserError(_("This partner is not linked to any user."))
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'partner.reset.password',
+            'view_mode': 'form',
+            'target': 'new',
+            'context': {
+                'default_user_ids': [(6, 0, self.user_ids.ids)],
+            },
+        }
---
+++ password_reset_partner/wizard/partner_reset_password.py
@@ -0,0 +1,25 @@
+from odoo import models, fields, _
+from odoo.exceptions import UserError
+
+
+class PartnerResetPassword(models.TransientModel):
+    _name = 'partner.reset.password'
+    _description = 'Wizard to reset password for a partner'
+
+    user_ids = fields.Many2many('res.users', string='Users', required=True, readonly=True)
+    new_password = fields.Char(string='New Password')
+    re_new_password = fields.Char(string='New Password (Retype)')
+
+    def action_reset_password(self):
+        if not self.user_ids:
+            raise UserError(_("No user selected for password reset."))
+        if not self.new_password or not self.re_new_password:
+            raise UserError(_("New password fields cannot be empty."))
+        if self.new_password != self.re_new_password:
+            raise UserError(_("The new passwords do not match."))
+        self.user_ids.write({'password': self.new_password})
+        for partner in self.user_ids.partner_id:
+            partner.message_post(
+                body=_("Password has been reset by %s", self.env.user.name)
+            )
+        return {'type': 'ir.actions.act_window_close'}
